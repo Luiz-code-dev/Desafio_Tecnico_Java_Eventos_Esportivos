f58c96bf1985142a46e8d974ad7e5bdd6d62170a
Luiz Eduardo <programadorlesd@gmail.com>
2021-07-22 22:18:28 -0300

    Inicio Project

---
+++ src/main/java/com/acpgroup/desafio/EventEsportApplication.java
@@ -0,0 +1,13 @@
+package com.acpgroup.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventEsportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventEsportApplication.class, args);
+	}
+
+}
